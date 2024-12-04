57e519141d40a9f81abce453063b7e7cb80f8cc0
kimalysha93 <kimalysha93@gmail.com>
2024-12-04 14:46:33 -0500

    deployment to github pages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-project-template
+
+IBM Full Stack Software Developer Professional Certificate: Developing Front-End Apps with React
+
+Final Project: Paradise Nursery Shopping Application
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -104,7 +104,7 @@ const CartItem = ({ onContinueShopping }) => {
           Continue Shopping
         </button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1">Coming Soon</button>
       </div>
     </div>
   );
